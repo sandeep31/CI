348ad3adcb051011f63d2188f873ef9f334eaa18
sandeep31 <wani.sandeep@gmail.com>
2017-06-28 13:58:26 +0530

    code  change on 1:58

--- index.php
+++ index.php
@@ -1,1 +1,90 @@
-first commit 
+start a working area (see also: git help tutorial)
+   clone      Clone a repository into a new directory
+   init       Create an empty Git repository or reinitialize an existing one
+
+work on the current change (see also: git help everyday)
+   add        Add file contents to the index
+   mv         Move or rename a file, a directory, or a symlink
+   reset      Reset current HEAD to the specified state
+   rm         Remove files from the working tree and from the index
+
+examine the history and state (see also: git help revisions)
+   bisect     Use binary search to find the commit that introduced a bug
+   grep       Print lines matching a pattern
+   log        Show commit logs
+   show       Show various types of objects
+   status     Show the working tree status
+
+grow, mark and tweak your common history
+   branch     List, create, or delete branches
+   checkout   Switch branches or restore working tree files
+   commit     Record changes to the repository
+   diff       Show changes between commits, commit and working tree, etc
+   merge      Join two or more development histories together
+   rebase     Reapply commits on top of another base tip
+   tag        Create, list, delete or verify a tag object signed with GPG
+
+collaborate (see also: git help workflows)
+   fetch      Download objects and refs from another repository
+   pull       Fetch from and integrate with another repository or a local branch
+   push       Update remote refs along with associated objects
+
+'git help -a' and 'git help -g' list available subcommands and some
+concept guides. See 'git help <command>' or 'git help <conce
+
+___________________________________
+available git commands in 'C:\Program Files\Git\mingw32/libexec/git-core'
+
+  add                 fsck-objects        rebase--helper
+  add--interactive    gc                  receive-pack
+  am                  get-tar-commit-id   reflog
+  annotate            grep                remote
+  apply               gui                 remote-ext
+  archimport          gui--askpass        remote-fd
+  archive             gui--askyesno       remote-ftp
+  askpass             gui.tcl             remote-ftps
+  bisect              hash-object         remote-http
+  bisect--helper      help                remote-https
+  blame               http-backend        repack
+  branch              http-fetch          replace
+  bundle              http-push           request-pull
+  cat-file            imap-send           rerere
+  check-attr          index-pack          reset
+  check-ignore        init                rev-list
+  check-mailmap       init-db             rev-parse
+  check-ref-format    instaweb            revert
+  checkout            interpret-trailers  rm
+  checkout-index      log                 send-email
+  cherry              ls-files            send-pack
+  cherry-pick         ls-remote           sh-i18n--envsubst
+  citool              ls-tree             shortlog
+  clean               mailinfo            show
+  clone               mailsplit           show-branch
+  column              merge               show-index
+  commit              merge-base          show-ref
+  commit-tree         merge-file          stage
+  config              merge-index         stash
+  count-objects       merge-octopus       status
+  credential          merge-one-file      stripspace
+  credential-manager  merge-ours          submodule
+  credential-store    merge-recursive     submodule--helper
+  credential-wincred  merge-resolve       subtree
+  cvsexportcommit     merge-subtree       svn
+  cvsimport           merge-tree          symbolic-ref
+  cvsserver           mergetool           tag
+  daemon              mktag               unpack-file
+  describe            mktree              unpack-objects
+  diff                mv                  update-index
+  diff-files          name-rev            update-ref
+  diff-index          notes               update-server-info
+  diff-tree           p4                  upload-archive
+  difftool            pack-objects        upload-pack
+  difftool--helper    pack-redundant      var
+  fast-export         pack-refs           verify-commit
+  fast-import         patch-id            verify-pack
+  fetch               prune               verify-tag
+  fetch-pack          prune-packed        web--browse
+  filter-branch       pull                whatchanged
+  fmt-merge-msg       push                worktree
+  for-each-ref        quiltimport         write-tree
+  format-patch        read-tree
